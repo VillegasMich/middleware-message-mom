00e7e0b3b3bfccce4f2e70ae3aa267fc99bd020d
VillegasMich <manuel.villegas.michel@gmail.com>
2025-03-16 19:38:10 -0500

    feat: list all and delete by id queues endpoints

--- server/app/routes/queue.py
+++ server/app/routes/queue.py
@@ -1,17 +1,26 @@
-from fastapi import APIRouter, Depends, HTTPException
-from sqlalchemy.orm import Session
-from app.models import Queue
 from app.core.database import get_db
+from app.models import Queue
+from fastapi import APIRouter, Depends, HTTPException
 from pydantic import BaseModel
+from sqlalchemy.orm import Session
 
 router = APIRouter()
+
 
 class QueueCreate(BaseModel):
     name: str
 
-@router.post("/queues/")
-def create_queue(queue: QueueCreate, db: Session = Depends(get_db)):
 
+@router.get("/queues/")
+async def get_queues(db: Session = Depends(get_db)):
+    queues = db.query(Queue).all()
+
+    return {"message": "Queues listed successfully", "queues": queues}
+
+
+# TODO: Add 'creator' created by user ...
+@router.post("/queues/")
+async def create_queue(queue: QueueCreate, db: Session = Depends(get_db)):
     existing_queue = db.query(Queue).filter(Queue.name == queue.name).first()
     if existing_queue:
         raise HTTPException(status_code=400, detail="Queue already exists")
@@ -22,3 +31,16 @@ def create_queue(queue: QueueCreate, db: Session = Depends(get_db)):
     db.refresh(new_queue)
 
     return {"message": "Queue created successfully", "queue_id": new_queue.id}
+
+
+# TODO: Add user 'creator' verification
+@router.delete("/queues/{queue_id}")
+async def delete_queue(queue_id: int, db: Session = Depends(get_db)):
+    deleted_queue = db.query(Queue).filter(Queue.id == queue_id).first()
+    if not deleted_queue:
+        raise HTTPException(status_code=404, detail="Queue not found")
+
+    db.delete(deleted_queue)
+    db.commit()
+
+    return {"message": "Queue deleted successfully", "queue_id": deleted_queue.id}
--- server/main.py
+++ server/main.py
@@ -1,10 +1,6 @@
-from fastapi import FastAPI, Depends
 from app.routes.queue import router as queue_router
+from fastapi import FastAPI
 
 app = FastAPI()
 
 app.include_router(queue_router)
-
-@app.get("/")
-def root():
-    return {"message": "Records successfully created"}
