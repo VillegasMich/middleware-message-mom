e9036d4adef06c86411692a8bc4c629d4c64cfc8
mvasquezb99 <mvasquezb@eafit.edu.co>
2025-04-01 13:19:50 -0500

    fix: modified server main.py and Listener

--- client/core/Listener.py
+++ client/core/Listener.py
@@ -33,18 +33,18 @@ class Listener:
         while True:
             try:
                 queues = User.get_user_queues()
-                print(f"Retrieved queues: {queues}")
+                # print(f"Retrieved queues: {queues}")
                 if queues and len(queues) > 0:
                     for queue in queues:
                         messages = Topic.pull_message(queue.get("id"))
-                        print(f"Messages received for queue {queue.get('id')}:", messages)
-
+                        # print(f"Messages received for queue {queue.get('id')}:", messages)
                         if messages:
                             for message in messages: 
                                 if queue["name"] in self.dict:
-                                    self.dict[queue["name"]].append(message)  
+                                    self.dict[queue["name"]].add(message)  
                                 else:
-                                    self.dict[queue["name"]] = [message] 
+                                    self.dict[queue["name"]] = {message} 
+                            
             except Exception as e:
                 print(f"Error al realizar la petición: {e}")
 
