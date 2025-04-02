c19469ed7f6d4111069b5b01dd7aa9c943158880
mvasquezb99 <mvasquezb@eafit.edu.co>
2025-04-02 09:10:16 -0500

    feat: Created MessageRepository for db management

--- server/app/grpc/Server.py
+++ server/app/grpc/Server.py
@@ -8,14 +8,11 @@ import grpc
 
 from . import Service_pb2, Service_pb2_grpc
 from ..core.database import get_db
-from ..models.message import Message
-from ..models.queue_message import QueueMessage
-from ..models.queue import Queue
-from ..models.queue_routing_key import QueueRoutingKey
+from ..repository.MessageRepository import MessageRepository
 # os.environ["GRPC_VERBOSITY"] = "debug"
 # os.environ["GRPC_TRACE"] = "all"
 
-GRPC_PORT = int(os.getenv("GRPC_PORT", 8080))  
+GRPC_PORT = int(os.getenv("GRPC_PORT", 8080))
 PUBLIC_IP = os.getenv("PUBLIC_IP")
 HOST = f"{PUBLIC_IP}:" + str(GRPC_PORT)
 
@@ -28,62 +25,65 @@ class MessageService(Service_pb2_grpc.MessageServiceServicer):
     """
 
     def AddMessage(self, request, context):
-        
+
         db = next(get_db())
-        
+        repo = MessageRepository(db)
+
         if request.type == 'queue':
-            new_message = Message(
-                content=request.content,
-                routing_key=request.routing_key,
-            )
+            repo.save_queue_message(request)
+            # new_message = Message(
+            #     content=request.content,
+            #     routing_key=request.routing_key,
+            # )
 
-            db.add(new_message)
-            db.flush()
+            # db.add(new_message)
+            # db.flush()
 
-            queue_message = QueueMessage(
-                queue_id=request.id, message_id=new_message.id
-            )
+            # queue_message = QueueMessage(
+            #     queue_id=request.id, message_id=new_message.id
+            # )
 
-            db.add(queue_message)
-            db.commit() 
+            # db.add(queue_message)
+            # db.commit()
 
         elif request.type == 'topic':
-            routing_key = request.routing_key
+            repo.save_topic_message(request)
+            # routing_key = request.routing_key
 
-            new_message = Message(
-                content=request.content,
-                routing_key=routing_key,
-                topic_id=request.id,
-            )
+            # new_message = Message(
+            #     content=request.content,
+            #     routing_key=routing_key,
+            #     topic_id=request.id,
+            # )
 
-            db.add(new_message)
-            db.flush()
+            # db.add(new_message)
+            # db.flush()
 
-            if not new_message.id:
-                raise HTTPException(status_code=500, detail="Failed to create message.")
+            # if not new_message.id:
+            #     raise HTTPException(status_code=500, detail="Failed to create message.")
 
-            all_queues = (
-                db.query(Queue)
-                .join(QueueRoutingKey, Queue.id == QueueRoutingKey.queue_id)
-                .filter(Queue.topic_id == request.id)
-                .all()
-            )
+            # all_queues = (
+            #     db.query(Queue)
+            #     .join(QueueRoutingKey, Queue.id == QueueRoutingKey.queue_id)
+            #     .filter(Queue.topic_id == request.id)
+            #     .all()
+            # )
 
-            matching_queues = [
-                queue
-                for queue in all_queues
-                if any(
-                    fnmatch.fnmatch(routing_key, qr.routing_key)
-                    for qr in queue.routing_keys
-                )
-            ]
+            # matching_queues = [
+            #     queue
+            #     for queue in all_queues
+            #     if any(
+            #         fnmatch.fnmatch(routing_key, qr.routing_key)
+            #         for qr in queue.routing_keys
+            #     )
+            # ]
 
-            queue_messages = [
-                QueueMessage(queue_id=queue.id, message_id=new_message.id)
-                for queue in matching_queues
-            ]
-            db.add_all(queue_messages)
-            db.commit()
+            # queue_messages = [
+            #     QueueMessage(queue_id=queue.id, message_id=new_message.id)
+            #     for queue in matching_queues
+            # ]
+            # db.add_all(queue_messages)
+            # db.commit()
 
         db.close()
         print("Request is received: " + str(request))
@@ -113,9 +113,9 @@ class Server:
     @staticmethod
     def listen():
         server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
-        Service_pb2_grpc.add_MessageServiceServicer_to_server(MessageService(), server)
+        Service_pb2_grpc.add_MessageServiceServicer_to_server(
+            MessageService(), server)
         server.add_insecure_port(HOST)
         print(f"Production service started on {HOST} ")
         server.start()
         server.wait_for_termination()
-
---
+++ server/app/repository/MessageRepository.py
@@ -0,0 +1,67 @@
+from fastapi import HTTPException
+from sqlalchemy.orm import Session
+from ..models.message import Message
+from ..models.queue import Queue
+from ..models.queue_message import QueueMessage
+from ..models.queue_routing_key import QueueRoutingKey
+
+import fnmatch
+
+
+class MessageRepository:
+
+    def __init__(self, db: Session):
+        self.db = db
+
+    def save_queue_message(self, request):
+        new_message = Message(content=request.content,
+                              routing_key=request.routing_key)
+
+        self.db.add(new_message)
+        self.db.flush()
+
+        queue_message = QueueMessage(
+            queue_id=request.id, message_id=new_message.id
+        )
+
+        self.db.add(queue_message)
+        self.db.commit()
+
+    def save_topic_message(self, request):
+        routing_key = request.routing_key
+
+        new_message = Message(
+            content=request.content,
+            routing_key=routing_key,
+            topic_id=request.id,
+        )
+
+        self.db.add(new_message)
+        self.db.flush()
+
+        if not new_message.id:
+            raise HTTPException(
+                status_code=500, detail="Failed to create message.")
+
+        all_queues = (
+            self.db.query(Queue)
+            .join(QueueRoutingKey, Queue.id == QueueRoutingKey.queue_id)
+                .filter(Queue.topic_id == request.id)
+                .all()
+        )
+
+        matching_queues = [
+            queue
+            for queue in all_queues
+            if any(
+                fnmatch.fnmatch(routing_key, qr.routing_key)
+                for qr in queue.routing_keys
+            )
+        ]
+
+        queue_messages = [
+            QueueMessage(queue_id=queue.id, message_id=new_message.id)
+            for queue in matching_queues
+        ]
+        self.db.add_all(queue_messages)
+        self.db.commit()
