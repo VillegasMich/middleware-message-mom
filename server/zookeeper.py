96cd813dc7d2de2b816b0ef8f5fe71c65df8f8fb
Manuela <mcastanof1@eafit.edu.co>
2025-04-12 21:24:47 -0500

    change local to cloud server zookeeper

--- server/zookeeper.py
+++ server/zookeeper.py
@@ -8,12 +8,12 @@ from app.models.user import User
 from kazoo.client import KazooClient
 from sqlalchemy.orm import Session
 
-ZK_HOST = "localhost:2181"  # LOCAL
-# ZK_HOST = "52.21.11.66:2181"  # EC2
+#ZK_HOST = "localhost:2181"  # LOCAL
+ZK_HOST = "52.21.11.66:2181"  # EC2
 # Server identification
 HOSTNAME = socket.gethostname()
-SERVER_IP = "127.0.0.1"  # LOCAL
-# SERVER_IP = os.getenv("SERVER_ELASTIC_IP")  # EC2
+#SERVER_IP = "127.0.0.1"  # LOCAL
+SERVER_IP = os.getenv("SERVER_ELASTIC_IP")  # EC2
 SERVER_PORT = int(os.getenv("SERVER_PORT", 8000))
 ZK_NODE_EPHEMERAL = f"/servers/{SERVER_IP}:{SERVER_PORT}"
 ZK_NODE_METADATA = f"/servers-metadata/{SERVER_IP}:{SERVER_PORT}"
